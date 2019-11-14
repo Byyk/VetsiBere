1dd53c4f7bd874ab424d8df984dfed101d096ad3
Byyk <38165149+Byyk@users.noreply.github.com>
2019-11-14 08:07:15 +0100

    -barvy

--- VetsiBere/Hra.Designer.cs
+++ VetsiBere/Hra.Designer.cs
@@ -28,133 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.consoleControl1 = new VetsiBere.Model.Components.ConsoleControl();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 285);
-            this.flowLayoutPanel1.TabIndex = 0;
-            this.flowLayoutPanel1.Click += new System.EventHandler(this.Hra_Click);
-            // 
-            // flowLayoutPanel2
-            // 
-            this.flowLayoutPanel2.AutoScroll = true;
-            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 303);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(3);
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(776, 165);
-            this.flowLayoutPanel2.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Plum;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.ForeColor = System.Drawing.Color.LightCoral;
-            this.button1.Image = global::VetsiBere.Properties.Resources.icons8_stop_32;
-            this.button1.Location = new System.Drawing.Point(794, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 50);
-            this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(797, 124);
-            this.trackBar1.Maximum = 19;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.trackBar1.Size = new System.Drawing.Size(45, 288);
-            this.trackBar1.TabIndex = 0;
-            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
-            this.trackBar1.Value = 15;
-            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
-            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
-            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
-            // 
-            // consoleControl1
-            // 
-            this.consoleControl1.BackColor = System.Drawing.Color.Bisque;
-            this.consoleControl1.Location = new System.Drawing.Point(850, 9);
-            this.consoleControl1.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
-            this.consoleControl1.Name = "consoleControl1";
-            this.consoleControl1.Size = new System.Drawing.Size(289, 459);
-            this.consoleControl1.TabIndex = 5;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Beige;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.PeachPuff;
-            this.button3.Image = global::VetsiBere.Properties.Resources.icons8_console_32;
-            this.button3.Location = new System.Drawing.Point(794, 418);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(50, 50);
-            this.button3.TabIndex = 4;
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.MediumSpringGreen;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.button2.Image = global::VetsiBere.Properties.Resources.icons8_play_32__1_;
-            this.button2.Location = new System.Drawing.Point(794, 68);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 50);
-            this.button2.TabIndex = 3;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
-            // 
-            // Hra
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(852, 480);
-            this.Controls.Add(this.consoleControl1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.flowLayoutPanel2);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Hra";
-            this.Text = "Hra";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Hra_FormClosing);
-            this.Load += new System.EventHandler(this.Hra_Load);
-            this.Shown += new System.EventHandler(this.Hra_Shown);
-            this.Click += new System.EventHandler(this.Hra_Click);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.components = new System.ComponentModel.Container();
+      this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+      this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+      this.button1 = new System.Windows.Forms.Button();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.trackBar1 = new System.Windows.Forms.TrackBar();
+      this.consoleControl1 = new VetsiBere.Model.Components.ConsoleControl();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // flowLayoutPanel1
+      // 
+      this.flowLayoutPanel1.AutoScroll = true;
+      this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+      this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+      this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+      this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+      this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 285);
+      this.flowLayoutPanel1.TabIndex = 0;
+      this.flowLayoutPanel1.Click += new System.EventHandler(this.Hra_Click);
+      // 
+      // flowLayoutPanel2
+      // 
+      this.flowLayoutPanel2.AutoScroll = true;
+      this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+      this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 303);
+      this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+      this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(3);
+      this.flowLayoutPanel2.Size = new System.Drawing.Size(776, 165);
+      this.flowLayoutPanel2.TabIndex = 1;
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.Plum;
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.button1.ForeColor = System.Drawing.Color.LightCoral;
+      this.button1.Image = global::VetsiBere.Properties.Resources.icons8_stop_32;
+      this.button1.Location = new System.Drawing.Point(794, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(50, 50);
+      this.button1.TabIndex = 2;
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.Button1_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+      // 
+      // trackBar1
+      // 
+      this.trackBar1.Location = new System.Drawing.Point(797, 124);
+      this.trackBar1.Maximum = 19;
+      this.trackBar1.Name = "trackBar1";
+      this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+      this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+      this.trackBar1.Size = new System.Drawing.Size(45, 288);
+      this.trackBar1.TabIndex = 0;
+      this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+      this.trackBar1.Value = 15;
+      this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
+      this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+      this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+      // 
+      // consoleControl1
+      // 
+      this.consoleControl1.BackColor = System.Drawing.Color.Bisque;
+      this.consoleControl1.Location = new System.Drawing.Point(850, 9);
+      this.consoleControl1.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+      this.consoleControl1.Name = "consoleControl1";
+      this.consoleControl1.Size = new System.Drawing.Size(289, 459);
+      this.consoleControl1.TabIndex = 5;
+      // 
+      // button3
+      // 
+      this.button3.BackColor = System.Drawing.Color.Beige;
+      this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3.ForeColor = System.Drawing.Color.PeachPuff;
+      this.button3.Image = global::VetsiBere.Properties.Resources.icons8_console_32;
+      this.button3.Location = new System.Drawing.Point(794, 418);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(50, 50);
+      this.button3.TabIndex = 4;
+      this.button3.UseVisualStyleBackColor = false;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button2
+      // 
+      this.button2.BackColor = System.Drawing.Color.MediumSpringGreen;
+      this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2.ForeColor = System.Drawing.Color.MediumSeaGreen;
+      this.button2.Image = global::VetsiBere.Properties.Resources.icons8_play_32__1_;
+      this.button2.Location = new System.Drawing.Point(794, 68);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(50, 50);
+      this.button2.TabIndex = 3;
+      this.button2.UseVisualStyleBackColor = false;
+      this.button2.Click += new System.EventHandler(this.Button2_Click);
+      // 
+      // Hra
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.Bisque;
+      this.ClientSize = new System.Drawing.Size(852, 480);
+      this.Controls.Add(this.consoleControl1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.trackBar1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.flowLayoutPanel2);
+      this.Controls.Add(this.flowLayoutPanel1);
+      this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "Hra";
+      this.Text = "Hra";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Hra_FormClosing);
+      this.Load += new System.EventHandler(this.Hra_Load);
+      this.Shown += new System.EventHandler(this.Hra_Shown);
+      this.Click += new System.EventHandler(this.Hra_Click);
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
